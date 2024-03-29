c2fc41ba58f8437cb7c0d5b5c9904380455d2fd6
Tim Macdonald <tim@tsmacdonald.com>
2024-03-29 16:10:43 +0000

    Basic tune in place

--- src/vamper/core.clj
+++ src/vamper/core.clj
@@ -1,5 +1,6 @@
 (ns vamper.core
   (:require
+   [clojure.math :as math]
    [overtone.live :refer :all]))
 
 (definst saw-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4]
@@ -24,6 +25,119 @@
         snd      (+ snd (mix [t1 t2 t3 t4]))]
     snd))
 
+(def pitch-ratio-a-minor
+  {:a 1
+   :b 9/8
+   :c 6/5
+   :d 4/3
+   :e 3/2
+   :f 8/5
+   :g 9/5})
+
+(def a-hz 440)
+
+(defn note->hertz
+  [[note octave]]
+  (let [octave-adjust (* a-hz (math/pow 2 (- octave 2)))]
+    (* octave-adjust (pitch-ratio-a-minor note))))
+
+(def rise-ye-lazy-lubber-bass
+  (let [pitches
+        (map note->hertz
+             (partition 2
+                        [:a 0 :a 0 :g -1 :g -1
+                         :a 0 :e -1 :a 0 :a 0
+                         :a 0 :a 0 :g -1 :g -1
+                         :a 0 :e -1 :a 0
+                         ;; A2
+                         :a 0 :a 0 :g -1 :g -1
+                         :a 0 :e -1 :a 0 :a 0
+                         :a 0 :a 0 :g -1 :g -1
+                         :a 0 :e -1 :a 0
+                         ;; B1
+                         :a 1 :b 1 :c 1 :c 1
+                         :e 1 :e 0 :a 1 :a 0
+                         :a 1 :b 1 :c 1 :c 1
+                         :d 1 :e 1 :a 1
+                         ;; B2
+                         :a 1 :b 1 :c 1 :d 1
+                         :e 1 :e 0 :a 1 :a 0
+                         :a 1 :b 1 :c 1 :c 1
+                         :d 1 :e 1 :a 1]))
+        durations
+        [2 2 2 2
+         2 2 2 2
+         2 2 2 2
+         2 2 4
+         ;; A2
+         2 2 2 2
+         2 2 2 2
+         2 2 2 2
+         2 2 4
+         ;; B1
+         2 2 2 2
+         2 2 2 2
+         2 2 2 2
+         2 2 4
+         ;; B2
+         2 2 2 2
+         2 2 2 2
+         2 2 2 2
+         2 2 4]
+        times (reductions + 0 durations)]
+    (map vector times pitches)))
+
+(def rise-ye-lazy-lubber-tune
+  (let [pitches
+        (map note->hertz
+             (partition 2
+                        [:a 2 :a 2 :b 2 :g 1 :a 2
+                         :c 2 :d 2 :e 2 :c 2 :a 2 :a 1
+                         :a 2 :a 2 :c 2 :g 1 :b 2
+                         :c 2 :d 2 :e 2 :c 2 :a 2
+                         ;; A2
+                         :a 2 :a 2 :b 2 :g 1 :a 2
+                         :c 2 :d 2 :e 2 :c 2 :a 2 :a 1
+                         :a 2 :a 2 :c 2 :g 1 :b 2
+                         :c 2 :d 2 :e 2 :c 2 :a 2 :g 2
+                         ;; B1
+                         :a 3 :e 2 :g 2 :d 2 :e 2 :d 2 :c 2 :a 3
+                         :g 2 :e 2 :d 2 :c 2 :a 2 :g 2
+                         :a 3 :e 2 :g 2 :d 2 :e 2 :d 2 :c 2 :e 2
+                         :d 2 :e 2 :g 2 :a 3 :a 2 :g 2
+                         ;; B2
+                         :a 3 :e 2 :g 2 :d 2 :e 2 :d 2 :c 2 :a 3
+                         :g 2 :e 2 :d 2 :c 2 :a 2 :g 2
+                         :a 3 :e 2 :g 2 :d 2 :e 2 :d 2 :c 2 :e 2
+                         :d 2 :e 2 :g 2 :a 3 :a 2]
+                        ))
+        durations
+        [2 1 1 3 1
+         1 1 1 1 2 2
+         2 1 1 3 1
+         1 1 1 1 4
+         ;; A2
+         2 1 1 3 1
+         1 1 1 1 2 2
+         2 1 1 3 1
+         1 1 1 1 3 1
+         ;; B1
+         1 1 1 1 1 1 1 1
+         1 1 1 1 3 1
+         1 1 1 1 1 1 1 1
+         1 1 1 1 3 1
+         ;; B2
+         1 1 1 1 1 1 1 1
+         1 1 1 1 3 1
+         1 1 1 1 1 1 1 1
+         1 1 1 1 4]
+        times (reductions + 0 durations)]
+    (map vector times pitches)))
+
+(let [tempo 108]
+  (play #_ saw-wave (metronome (* 4 tempo)) rise-ye-lazy-lubber-tune)
+  (play #_ saw-wave (metronome (* 4 tempo)) rise-ye-lazy-lubber-bass))
+
 (def melody
   (let [pitches
         [67 67 67 69 71                 ; Row, row, row your boat,
@@ -41,7 +155,7 @@
 (defn play
   ([metro notes] (play harpsichord metro notes))
   ([instrument metro notes]
-   (let [play-note (fn [[beat midi]] (at (metro beat) (-> midi midi->hz instrument)))]
+   (let [play-note (fn [[beat midi]] (at (metro beat) (-> midi instrument)))]
      (dorun (map play-note notes)))))
 
 (defn after [beats metro] (comp metro #(+ % beats)))
